8a8fc2913d6f12345c3b44ca81e20ce9de948da7
kundapur.a@northeastern.edu <kundapur.a@northeastern.edu>
2025-10-07 16:59:17 +0000

    final1

--- tpch_medallion_dbt/models/gold/dim_part.sql
+++ tpch_medallion_dbt/models/gold/dim_part.sql
@@ -1,6 +1,6 @@
 with base as (select * from {{ ref('dim_part_s') }})
 select
-  {{ dbt_utils.surrogate_key(['part_key']) }} as part_sk,
+  {{ dbt_utils.generate_surrogate_key(['part_key']) }} as part_sk,
   part_key     as part_nk,
   part_name,
   mfgr,
